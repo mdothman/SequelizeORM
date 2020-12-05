38d88f1da7d6d3144ad75c7b9b95fb181c553a8c
BEELZ <othmanmuhidean@gmail.com>
2020-12-04 17:42:38 -0800

    I have a basic setup for a full-stack project without client

---
+++ server.js
@@ -0,0 +1,21 @@
+const express  = require("express");
+const db = require("./models");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+}
+
+app.use(routes)
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
